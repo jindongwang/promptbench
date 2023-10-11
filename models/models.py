3c689f6330071f1d818667e8fb44416e91ecf4c3
Jindong Wang <jindongwang@outlook.com>
2023-10-11 05:02:27 -0700

    upd: code

--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+import os
 
 class LMMBaseModel(object):
     
@@ -20,14 +21,17 @@ class LMMModel(object):
 
     def __init__(self, **kwargs):
         self.model = kwargs.get('model', None)
+        self.infer_model = self.create_model(**kwargs)
         
     def create_model(self, **kwargs):
         if self.model == 'google/flan-t5-large':
             return T5Model(**kwargs)
+        elif self.model == 'llama2-7b':
+            return LlamaModel(**kwargs)
         return None
 
     def __call__(self, **kwargs):
-        return self.create_model(**kwargs)
+        return self.infer_model
 
 
 class T5Model(LMMBaseModel):
@@ -50,8 +54,32 @@ class T5Model(LMMBaseModel):
         return out
 
 
+class LlamaModel(LMMBaseModel):
+    
+    def __init__(self, **kwargs):
+        super(LlamaModel, self).__init__(**kwargs)
+        model_dir = kwargs.get('model_dir', None)
+        if not model_dir:
+            raise ValueError("model_dir is required for llama model!")
+        
+        from transformers import LlamaForCausalLM, LlamaTokenizer
+
+        self.tokenizer = LlamaTokenizer.from_pretrained(
+            model_dir, device_map="auto")
+        self.pipe = LlamaForCausalLM.from_pretrained(
+            model_dir, device_map="auto")
+        
+    def predict(self, input_text):
+        input_ids = self.tokenizer(
+            input_text, return_tensors="pt").input_ids.to("cuda")
+        outputs = self.pipe.generate(
+                input_ids, max_length=self.gen_len)
+        out = self.tokenizer.decode(outputs[0])
+        return out
+
 
 if __name__ == '__main__':
-    model = LMMModel(model='google/flan-t5-large', gen_len=5)()
+    model = LMMModel(model='llama2-7b', gen_len=5, model_dir='home/jindwang/mine/llama2-70b')()
     print(type(model))
+    print()
     print(model.predict('The quick brown fox jumps over the lazy dog'))
